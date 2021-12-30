e617a3fc4d615dbda1c49a44e42ed9390944d05d
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2021-12-30 16:31:09 +0530

    Data types

---
+++ js/tut4.js
@@ -0,0 +1,21 @@
+// primitive data types:
+
+//string
+var nam='ddd';
+console.log('my string is '+nam)
+console.log('Data Type is '+ (typeof nam));
+
+arr=[1,2,3,'anmol']
+console.log("Data type is "+(typeof arr));
+
+let stMarks={
+    harry:89,
+    coh:100,
+    'Ram Lal: ':90
+}
+
+console.log(typeof stMarks);
+
+let date=new Date();
+console.log(typeof date);
+
