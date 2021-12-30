106af9845f82c93230dedfec4ba7cbd5676e0b67
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2021-12-30 15:52:27 +0530

    console

---
+++ js/index.js
@@ -0,0 +1,9 @@
+console.time('Your code took: ')
+console.log("Hello Console");
+console.log(4+34);
+console.log([3,4,5,6]);
+console.log({addiee:'this',marks:40})
+console.table({addiee:'this',marks:40})
+console.warn('Warning');
+console.log({addiee:'this',marks:40})
+console.timeEnd('Your code took: ')
