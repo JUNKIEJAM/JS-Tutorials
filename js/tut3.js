0def0527ac736fa6d85772e6c0d4e94c3088ce18
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2021-12-30 16:08:56 +0530

    var let const

---
+++ js/tut3.js
@@ -0,0 +1,26 @@
+console.log('tut3');
+
+var nam= "addiee";
+var channel;
+channel="code with me";
+
+var marks=34;
+
+console.log(nam);
+
+const c='lol'
+console.log(c)
+
+let city='Delhi'
+console.log(city)
+
+{
+    let city='Rampur'
+    console.log(city)
+}
+
+console.log(city)
+
+const arr1=[12,23,45,33];
+arr1.push(45);
+console.log(arr1)
