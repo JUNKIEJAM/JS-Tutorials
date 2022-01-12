31f2d190deac92f71f01ae3957a5cd6b508391d1
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2022-01-12 23:42:18 +0530

    Error Handling

---
+++ js/tut44.js
@@ -0,0 +1,20 @@
+console.log("Tutorial 44");
+
+let a = "bhayia";
+a = undefined;
+if (a != undefined) {
+    throw new Error("Not Undefined");
+} else {
+    console.log("Undefined");
+}
+
+
+try {
+    //k kj j j kj 
+    console.log("Try block");
+
+} catch (error) {
+    console.log("Are you okay? ");
+} finally {
+    console.log("RUN");
+}
